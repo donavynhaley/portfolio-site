f6322154e1fe86a2a029b9100b770c2665d79e9a
sirbaconsalot <donavynhaley@gmail.com>
2021-06-08 15:50:19 -0700

    updated navbar links to glow when related seection is navigated to by the user

--- portfolio/src/sections/About.js
+++ portfolio/src/sections/About.js
@@ -6,7 +6,8 @@ const About = () => {
             <div className="about-container">
                 <h2 className="section-heading text-white"> about </h2>
                 <Divider isDark={false} />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit egestas dui id ornare arcu odio ut sem nulla. Pharetra magna ac placerat vestibulum. Enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit. Felis donec et odio pellentesque diam. Leo a diam sollicitudin tempor id. Imperdiet sed euismod nisi porta. Scelerisque varius morbi enim nunc faucibus a pellentesque. Nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Accumsan in nisl nisi scelerisque. Et magnis dis parturient montes nascetur. Placerat vestibulum lectus mauris ultrices eros in cursus turpis massa. Ipsum suspendisse ultrices gravida dictum fusce ut placerat. Neque gravida in fermentum et sollicitudin. Imperdiet sed euismod nisi porta lorem mollis aliquam. Habitant morbi tristique senectus et netus et malesuada.</p>
+                <p>I'm a Full-Stack Developer located in Vancouver, WA. I have a serious passion for creating products that help people and make a difference.
+                Well-organized person, an independent employee who loves problem-solving. Fan of 3D printing, gaming, woodworking, and archery.Interested in the entire development spectrum and working on ambitious projects.</p>
             </div>
         </div>
     )
--- portfolio/src/sections/Navbar.js
+++ portfolio/src/sections/Navbar.js
@@ -1,14 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import Button from '../common/Button'
 import resume from '../assets/resume.pdf'
+
 const Navbar = () => {
     const [shrink, setShrink] = useState(false);
+    const [activePortfolio, setActivePortfolio] = useState(false);
+    const [activeAbout, setActiveAbout] = useState(false);
+    const [activeContact, setActiveContact] = useState(false);
+
     // shrinks navbar if user scrolls down
     useEffect(() => {
         window.addEventListener('scroll', () => {
             setShrink(window.scrollY > 0);
+
+
+            const top = window.pageYOffset;
+            const windowHeight = window.innerHeight;
+            const docHeight = getDocHeight();
+
+            const totalDocScrollLength = docHeight - windowHeight;
+            const scrollPostion = Math.floor(top / totalDocScrollLength * 100);
+            console.log(scrollPostion)
+            // 18 69 85
+            const portfolioPage = 18;
+            const aboutPage = 69;
+            const contactPage = 85;
+            setActivePortfolio(scrollPostion >= portfolioPage && scrollPostion < aboutPage)
+            setActiveAbout(scrollPostion >= aboutPage && scrollPostion < contactPage)
+            setActiveContact(scrollPostion >= contactPage)
         })
     })
+
+    // gets max for total docheight
+    const getDocHeight = () => {
+        return Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+        );
+    }
 
     return (
         <nav className={`nav-container ${shrink ? "navbar-shrink" : ""} `} id="#mainNav">
@@ -16,12 +46,12 @@ const Navbar = () => {
                 <a className="name" href="#home">Donavyn Haley</a>
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href="#portfolio">Portfolio</a>
+                        <a className={`nav-link ${activePortfolio ? "active" : ""}`} href="#portfolio">Portfolio</a>
                     </li>
-                    <li className="nav-item">
+                    <li className={`nav-link ${activeAbout ? "active" : ""}`}>
                         <a className="nav-link" href="#about">About</a>
                     </li>
-                    <li className="nav-item">
+                    <li className={`nav-link ${activeContact ? "active" : ""}`}>
                         <a className="nav-link" href="#contact">Contact</a>
                     </li>
                     <li className="nav-item" id="resume">
