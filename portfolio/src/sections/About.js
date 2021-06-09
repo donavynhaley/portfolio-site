f324700175aaa463a752923bc837b5a03953f5c8
sirbaconsalot <donavynhaley@gmail.com>
2021-06-09 10:47:44 -0700

    Updated arrow styles for modal. Added missing space in about page

--- portfolio/src/sections/About.js
+++ portfolio/src/sections/About.js
@@ -7,7 +7,7 @@ const About = () => {
                 <h2 className="section-heading text-white"> about </h2>
                 <Divider isDark={false} />
                 <p>I'm a Full-Stack Developer located in Vancouver, WA. I have a serious passion for creating products that help people and make a difference.
-                Well-organized person, an independent employee who loves problem-solving. Fan of 3D printing, gaming, woodworking, and archery.Interested in the entire development spectrum and working on ambitious projects.</p>
+                Well-organized person, an independent employee who loves problem-solving. Fan of 3D printing, gaming, woodworking, and archery. Interested in the entire development spectrum and working on ambitious projects.</p>
             </div>
         </div>
     )
